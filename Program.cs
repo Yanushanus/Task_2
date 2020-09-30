a5de2c562d5a23cf9898f18daf23ffb564c6639d
=Yanush <giroskop976@gmail.com>
2020-09-30 10:03:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your c: ");
+            long c = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine($" Your A= {a}\n Your B= {b}\n Your C= {c}");
+        }
+    }
+}
